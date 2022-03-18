4a865b72bf433bf0d6f087f8abe454ceb358e90c
Lucus Riccioni <lucusroni@gmail.com>
2022-03-18 04:54:38 +0000

    Index file for ISS

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
 const { nextISSTimesForMyLocation, } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// })
-
-// fetchCoordsByIP("207.228.78.67", (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned Coordinates:' , coordinates);
-// })
-
-// fetchISSFlyOverTimes("207.228.78.67", (error, coords) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-  
-//   console.log('It worked! Returned Flyover Times:' , coords);
-// });
-
 const printPassTimes  = function(passTimes) {
   for (let pass of passTimes) {
     let datetime = new Date(0);
